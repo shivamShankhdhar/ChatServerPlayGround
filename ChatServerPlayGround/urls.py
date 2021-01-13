f33e6fd8ed72b59826215ade6693577259473a46
shivam shankhdhar <s.shankhdhar1981@gmail.com>
2021-01-13 20:27:49 +0530

    login logout view added

--- ChatServerPlayGround/urls.py
+++ ChatServerPlayGround/urls.py
@@ -8,12 +8,17 @@ from personal.views import (
 )
 from account.views import (
 		register_view,
+		login_view,
+		logout_view,
 	)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home_screen_view, name = "home"),
+    path('admin/', admin.site.urls),
+    path('login/', login_view, name = "login"),
+    path('logout/', logout_view, name = "logout"),
     path('register/', register_view, name = "register"),
+
 ]
 
 if settings.DEBUG:
--- account/views.py
+++ account/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 
 
@@ -25,7 +25,7 @@ def register_view(request, *args, **kwargs):
 			account = authenticate(email = email, password= raw_password)
 			login(request, account)
 			messages.success(request, f"Logged In as ' {email} '.")
-			destination = kwargs.get("next") # grabbing the requested page when user was not authenticated
+			destination = get_redirect_if_exists(request) # grabbing the requested page when user was not authenticated
 			if destination:
 				return redirect(destination)
 				# messages.info(request, f'Logged In as {email}.')
@@ -36,3 +36,42 @@ def register_view(request, *args, **kwargs):
 			context['registration_form'] = form
 
 	return render(request, template_name, context)
+
+
+
+def login_view(request, *args, **kwargs):
+	template_name = 'account/login.html'
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		return redirect("home")
+	if request.POST:
+		form = AccountAuthenticationForm(request.POST)
+		if form.is_valid():
+			email 		= request.POST['email']
+			password 	= request.POST['password']
+			user = authenticate(email = email, password = password)
+			if user:
+				login(request, user)
+				messages.success(request, f"User logged in as ' {email}' ")
+				destination = get_redirect_if_exists(request)
+				if destination:
+					return redirect(destination)
+				return redirect("home")
+		else:
+			context['login_form'] = form
+
+	return render(request, template_name, context)
+
+def get_redirect_if_exists(request):
+	redirect = None
+	if request.GET:
+		if request.GET.get("next"):
+			redirect = str(request.GET.get("next"))
+	return redirect
+
+def logout_view(request):
+	logout(request)
+	messages.success(request, "User Logged out Successfully .")
+	return redirect("home")
+
